56b509fc9712e35c1c0ccae816e827417ce86bc5
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-02-14 14:36:00 +0530

    feat(day2): add while-loop program to compute sum of n natural numbers

---
+++ src/com/day2/SumOfNaturalNumbersWhile.java
@@ -0,0 +1,18 @@
+package com.day2;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumbersWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+        int sum = 0;
+        while (n > 0) {
+            sum = sum + n;
+            n--;
+        }
+        System.out.println("The sum is " + sum);
+    }
+}
+
