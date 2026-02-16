383ca8dac0139a17a42fea1a5d39c151e1a939cc
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-02-16 14:36:18 +0530

    feat(day3): add program to sort array in ascending order

---
+++ src/com/day3/ArraySortAscending.java
@@ -0,0 +1,32 @@
+package com.day3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySortAscending {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sort(arr);
+        System.out.println("Sorted Array: ");
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+    }
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+    }
+}
