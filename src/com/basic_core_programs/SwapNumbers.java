48caf0dca6e415f5a04302a3bd4e7006eea4c2d3
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-02-26 14:44:25 +0530

    Addes SwapNumbers program

---
+++ src/com/basic_core_programs/SwapNumbers.java
@@ -0,0 +1,19 @@
+package com.basic_core_programs;
+
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("Before Swap: a=" + a + " b=" + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After Swap: a=" + a + " b=" + b);
+    }
+}
