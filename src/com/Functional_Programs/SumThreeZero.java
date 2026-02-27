3fc9f0a58af8db23014be5f0b9408baa46be765d
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-02-27 10:42:14 +0530

    Added Sum of Three Integers Equals Zero program

---
+++ src/com/Functional_Programs/SumThreeZero.java
@@ -0,0 +1,37 @@
+package com.Functional_Programs;
+/*
+Sum of three Integer adds to ZERO
+a. Desc -> A program with cubic running time. Read in N integers and counts the
+number of triples that sum to exactly 0.
+b. I/P -> N number of integer, and N integer input array
+c. Logic -> Find distinct triples (i, j, k) such that a[i] + a[j] + a[k] = 0
+d. O/P -> One Output is number of distinct triplets as well as the second output is to
+print the distinct triplets.
+ */
+import java.util.Scanner;
+
+public class SumThreeZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        int count = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        System.out.println(arr[i] + " " + arr[j] + " " + arr[k]);
+                        count++;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Total Distinct Triplets: " + count);
+    }
+}
